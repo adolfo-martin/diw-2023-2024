326929d146b0dfb8f04739e597aecdf73b0c6bc9
adolfo-martin <adolfo.martin@iesramonarcas.es>
2024-01-16 14:29:09 +0100

    hoy

---
+++ U.T. 6. Framework tailwindcss/2024-01-16 before y after/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        'red': '#ff0000',
+        'yellow': '#ffff00'
+      }
+    },
+  },
+  plugins: [],
+}
+
